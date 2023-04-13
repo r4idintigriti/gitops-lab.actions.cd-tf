edf532c4eb9aed71199e185402643703975fc114
r4idintigriti <127827662+r4idintigriti@users.noreply.github.com>
2023-04-13 16:16:26 +0200

    Update deployVM.tf

--- deployVM.tf
+++ deployVM.tf
@@ -1,14 +1,35 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name   = "DevOpsConfig"
-    storage_account_name  = "saTerraform"
-    container_name        = "terraform"
-    key                   = "terraform.tfstate"
+  backend "remote" {
+    organization = "jverheyen"
+ 
+    workspaces {
+      name = "jverheyen-workspace"
+    }
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
   }
 }
 
 provider "azurerm" {
-    features { }
+  features {}
+ 
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+variable "client_id" {
+}
+variable "client_secret" {
+}
+variable "subscription_id" {
+}
+variable "tenant_id" {
 }
 
 variable "RG" {
@@ -27,14 +48,7 @@ variable "SUBNET_ADDRESS" {
     default = "10.10.0.0/24"
 }
 
-data "azurerm_key_vault" "keyvault" {
-    name = "kvHRSConfig"
-    resource_group_name = "DevOpsConfig" 
-}
-
-data "azurerm_key_vault_secret" "adminpwd" {
-    name = "adminpwd"
-    key_vault_id = data.azurerm_key_vault.keyvault.id
+variable "admin_pwd" {
 }
 
 resource "azurerm_resource_group" "RG" {
@@ -139,7 +153,7 @@ resource "azurerm_virtual_machine" "VM1" {
     os_profile {
       computer_name = "VM1"
       admin_username = "u2uadmin"
-      admin_password = data.azurerm_key_vault_secret.adminpwd.value
+      admin_password = var.admin_pwd
     }
 
     os_profile_windows_config {
